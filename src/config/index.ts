8d64352ec4a2ff417c07616ac626457a4254f4e0
HunteRoi <me@tinaeldevresse.eu>
2022-08-17 22:25:52 +0200

    feat: push base config

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSAGING_SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "MEASUREMENT_ID"
+};
+
+export const facebookConfig = {
+    appId: "APP_ID",
+    appSecret: "APP_SECRET"
+};
